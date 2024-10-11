e269964b6d71c87c3d1f104aaeaac659f842e673
Rom-haguko <romagureev170@gmail.com>
2024-10-11 14:51:06 +0300

    Добавьте файлы проекта.

---
+++ zad4_coord/zad4_coord.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>;
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	double a, b, c;
+	cout << "Введите параметр a, b и c: ";
+	cin >> a >> b >> c;
+	double x;                         // находим x вершину
+	x = -b / (2 * a);
+	double y;
+	y = a * pow(x,2) + b * x + c;
+	cout << "Полученные координаты вершины (x,y): "<< x<<", "<< y;
+	
+	
+}
+
